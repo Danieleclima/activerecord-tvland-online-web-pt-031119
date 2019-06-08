6fb2a81a323f7e60401356d0fc3e29b883a260db
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-08 13:28:05 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_day_and_genre_to_shows.rb
+++ db/migrate/009_add_day_and_genre_to_shows.rb
@@ -3,6 +3,7 @@ class AddDayAndGenreToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
+    add_colum :shows, :season, :string
   end
   
 end
