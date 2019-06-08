2f245a657187f3ea7b322617a789c6d6ba0bca0e
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-08 12:52:05 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_catchphrase_to_character.rb
+++ db/migrate/008_add_catchphrase_to_character.rb
@@ -1,6 +1,5 @@
-class AddShowToCharacters < ActiveRecord::Migration[4.2]
-
+class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :show_id, :integer
+    add_column :characters, :catchphrase, :string
   end
 end
