20795cf9effdad6233f13d732870b4695d1d88eb
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-08 13:18:30 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_network_to_character.rb
+++ db/migrate/009_add_network_to_character.rb
@@ -1,5 +1,5 @@
 class AddNetworkToCharacter < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :characters, :network_id, :string
   end
 end
